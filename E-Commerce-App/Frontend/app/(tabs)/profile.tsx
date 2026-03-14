80e3059ce6ea1de26da74423eea623d19cf110e5
Suraj Gupta <surajgupta7070031833@gmail.com>
2026-03-15 01:55:32 +0530

    Frontend UI Completed

--- E-Commerce-App/Frontend/app/(tabs)/profile.tsx
+++ E-Commerce-App/Frontend/app/(tabs)/profile.tsx
@@ -5,14 +5,15 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '@/components/Header';
 import { Ionicons } from '@expo/vector-icons';
 import { COLORS, PROFILE_MENU } from '@/constants';
-import { dummyUser } from '@/assets/assets';
+import { useClerk } from '@clerk/expo';
 
 export default function Profile() {
 
-    const { user } = { user: dummyUser };
+    const { user, signOut } = useClerk();
     const router = useRouter();
 
     const handleLogout = async () => {
+        await signOut();
         router.push('/sign-in');
     };
 
