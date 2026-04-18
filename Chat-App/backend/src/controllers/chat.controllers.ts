1efaa46cce14f90a0453fdaac2dc7853397ad6ea
Suraj Gupta <surajgupta7070031833@gmail.com>
2026-04-18 22:31:05 +0530

    feat: implement chat and message retrieval, and user listing endpoints

---
+++ Chat-App/backend/src/controllers/chat.controllers.ts
@@ -0,0 +1,77 @@
+import type { Response, NextFunction } from "express";
+import type { AuthRequest } from "../middleware/auth";
+import { Chat } from "../models/chat.models";
+
+export async function getChats(req: AuthRequest, res: Response, next: NextFunction) {
+    try {
+        const userId = req.userId;
+
+        const chats = await Chat.find({ participants: userId })
+            .populate("participants", "name email avatar")
+            .populate('lastMessages')
+            .sort({ lastMessagesAt: -1 });
+
+        const formattedChats = chats.map(chat => {
+            const otherParticipant = chat.participants.find(
+                (p) => p._id.toString() !== userId
+            );
+
+            return {
+                _id: chat._id,
+                participants: otherParticipant,
+                lastMessages: chat.lastMessages,
+                lastMessagesAt: chat.lastMessagesAt,
+                createdAt: chat.createdAt
+            };
+        });
+
+        res.json({
+            success: true,
+            chats: formattedChats
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export async function getOrCreateChat(req: AuthRequest, res: Response, next: NextFunction) {
+    try {
+        const userId = req.userId;
+        const { participantId } = req.params;
+
+        // Check if chat already exists
+        let chat = await Chat.findOne({
+            participants: {
+                $all: [userId, participantId]
+            }
+        })
+            .populate("participants", "name email avatar")
+            .populate('lastMessages');
+
+        if (!chat) {
+            const newChat = new Chat({
+                participants: [userId, participantId]
+            });
+            await newChat.save();
+            chat = await newChat.populate("participants", "name email avatar");
+        }
+
+        const otherParticipant = chat.participants.find(
+            p => p._id.toString() !== userId
+        );
+
+        res
+            .status(200)
+            .json({
+                success: true,
+                _id: chat._id,
+                participants: otherParticipant,
+                lastMessages: chat.lastMessages,
+                lastMessagesAt: chat.lastMessagesAt,
+                createdAt: chat.createdAt
+            });
+    } catch (error) {
+        res.status(500)
+        next(error);
+    }
+}
