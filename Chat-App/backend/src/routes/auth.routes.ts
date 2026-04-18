94192284bc5d82e48620d136ad0a3983bc2447cd
Suraj Gupta <surajgupta7070031833@gmail.com>
2026-04-18 20:31:05 +0530

    feat: integrate Clerk authentication and update dependencies in backend and web

--- Chat-App/backend/src/routes/auth.routes.ts
+++ Chat-App/backend/src/routes/auth.routes.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { protectRoute } from "../middleware/auth";
+import { authCallBack, getMe } from "../controllers/auth.controllers";
 
 const AuthRouter = Router();
+
+AuthRouter.get("/me", protectRoute, getMe);
+AuthRouter.post('/callback', authCallBack);
 
 export default AuthRouter;
