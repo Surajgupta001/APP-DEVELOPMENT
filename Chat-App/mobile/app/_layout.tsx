0c82f1c41bffd0e71e36017c4bdc3e315f372aa3
Suraj Gupta <surajgupta7070031833@gmail.com>
2026-04-19 20:51:39 +0530

    feat: initialize mobile app with Expo, Tailwind CSS, and NativeWind
    - Add ESLint configuration for code linting.
    - Create global CSS file for Tailwind CSS styles.
    - Set up Metro bundler configuration to integrate NativeWind.
    - Add TypeScript declaration file for NativeWind.
    - Initialize package.json with dependencies and scripts for mobile app.
    - Configure Tailwind CSS with paths to components and app files.
    - Set up TypeScript configuration extending Expo's base config.

---
+++ Chat-App/mobile/app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+import '../global.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
+
+export default function RootLayout() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack screenOptions={{
+        headerShown: false
+      }} />
+    </QueryClientProvider>
+  );
+}
