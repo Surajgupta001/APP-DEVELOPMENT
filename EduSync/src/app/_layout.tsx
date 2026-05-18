77b70db3f9e77012301f9d381fabe626eea5fe37
Suraj Gupta <surajgupta7070031833@gmail.com>
2026-05-18 13:47:10 +0530

    feat: update dependencies and enhance profile screen
    - Added new dependencies: @react-native-community/netinfo, @sentry/react-native, expo-image-manipulator, react-native-svg, react-native-teleport, stream-chat-expo.
    - Refactored ProfileScreen component to include user information, profile picture, and a sign-out button with error handling using Sentry.
    - Implemented a menu with navigation options and user statistics.
    - Initialized Sentry for error tracking and logging.
    - Created API endpoints for syncing user data with Stream Chat and generating tokens.
    - Added a theme file for consistent styling across the application.

--- EduSync/src/app/_layout.tsx
+++ EduSync/src/app/_layout.tsx
@@ -2,7 +2,28 @@ import { Stack } from "expo-router";
 import '../../global.css';
 import { ClerkProvider } from '@clerk/expo'
 import { tokenCache } from '@clerk/expo/token-cache'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import * as Sentry from '@sentry/react-native';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
+Sentry.init({
+  dsn: 'https://d4b8d747c91465fb4555112d5acb79f5@o4511401321299968.ingest.us.sentry.io/4511409567563776',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Enable Logs
+  enableLogs: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
@@ -11,10 +32,14 @@ if (!publishableKey) {
 }
 
 export default function RootLayout() {
-  return <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache} >
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(auth)" />
-      <Stack.Screen name="(tabs)" />
-    </Stack>
-  </ClerkProvider>;
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache} >
+      <GestureHandlerRootView className="flex-1">
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="(auth)" />
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+      </GestureHandlerRootView>
+    </ClerkProvider>
+  );
 }
