f8c9c6a6c5d0efa9254aff094ffdc94a42cab6f3
Suraj Gupta <surajgupta7070031833@gmail.com>
2026-05-20 00:05:30 +0530

    REMOVED

--- EduSync/src/app/(tabs)/explore.tsx
+++
@@ -1,9 +0,0 @@
-import { StyleSheet, Text, View } from 'react-native'
-
-export default function ExploreScreen() {
-    return (
-        <View>
-            <Text>ExploreScreen</Text>
-        </View>
-    )
-}
--- EduSync/src/app/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { StyleSheet, Text, View } from 'react-native'
-
-export default function ChatScreen() {
-    return (
-        <View>
-            <Text>ChatScreen</Text>
-        </View>
-    )
-}
