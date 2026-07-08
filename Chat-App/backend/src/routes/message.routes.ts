1e7a62d7b9195546865f2f26e14fddf848af4416
surajgupta001 <surajgupta7070031833@gmail.com>
2026-07-08 20:30:33 +0530

    All Things Removed

--- Chat-App/backend/src/routes/message.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { protectRoute } from "../middleware/auth";
-import { getMessages } from "../controllers/message.controllers";
-
-const MessagesRouter = Router();
-
-MessagesRouter.get('/get/:chatId', protectRoute, getMessages);
-
-export default MessagesRouter;
